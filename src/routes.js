c52560ae4ca0248962b6e8594181f32ac75c5b65
Alex Sodre <skynetsistema2020@gmail.com>
2020-04-19 18:49:56 -0300

    loader models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Sodre' }));
-
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Alex Sodré',
+    email: 'iconexaoweb@gmail.com',
+    password_hash: '123456789',
+  });
+return res.json(user);
+});
 export default routes;
