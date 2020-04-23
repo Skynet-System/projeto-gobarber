10bb179bcee2596833efa1967dd6fcdc205104ec
Alex Sodre <skynetsistema2020@gmail.com>
2020-04-22 22:00:52 -0300

    validando entrada

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,9 +1,20 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
+
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SeesionController {
   async store(req, res) {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+        .email()
+        .required(),
+        password: Yup.string().required(),
+      });
+      if(!(await schema.isValid(req.body))) {
+        return res.status(400).json({ error: 'Validation fails' });
+      }
     const { email, password } = req.body;
     const user = await User.findOne({ where: { email } });
 
@@ -23,7 +34,7 @@ class SeesionController {
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
-    }); 
+    });
   }
 }
 
